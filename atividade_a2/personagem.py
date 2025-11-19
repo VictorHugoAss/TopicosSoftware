7a29a9fc71bd0cea87f009de080ac43ae2837a0b
Victor Assunção <143351429+VictorHugoAss@users.noreply.github.com>
2025-11-19 09:01:59 -0300

    Implementando "to_dict"
    Adicionando o método "to_dict" para o salvamento em json

--- atividade_a2/personagem.py
+++ atividade_a2/personagem.py
@@ -18,3 +18,28 @@ Alinhamento: {self.raca.alinhamento}
 Habilidades Raciais: {", ".join(self.raca.habilidades())}
 Habilidades da Classe: {", ".join(self.classe.habilidades())}
 """
+    
+    def to_dict(self):
+        """Converte o objeto Personagem em um dicionário serializável em JSON."""
+        return {
+            "nome": self.nome,
+            "raca": {
+                "tipo": self.raca.__class__.__name__,
+                "movimento": self.raca.movimento,
+                "infravisao": self.raca.infravisao,
+                "alinhamento": self.raca.alinhamento,
+                "habilidades": self.raca.habilidades()
+            },
+            "classe": {
+                "tipo": self.classe.__class__.__name__,
+                "habilidades": self.classe.habilidades()
+            },
+            "atributos": {
+                "forca": self.atributos.forca,
+                "destreza": self.atributos.destreza,
+                "constituicao": self.atributos.constituicao,
+                "inteligencia": self.atributos.inteligencia,
+                "sabedoria": self.atributos.sabedoria,
+                "carisma": self.atributos.carisma
+            }
+        }
